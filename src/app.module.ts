09866686db2946e54be14347b4c49df05da1f047
chrisMJ <chris666.10@hotmail.com>
2024-03-20 18:27:25 -0500

    conexion a mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import {ConfigModule} from '@nestjs/config'; 
+import { MongooseModule }  from '@nestjs/mongoose'; 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.urimongo),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
