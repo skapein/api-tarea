49728394baa99059b46bdca9921e688b7d267fb0
chrisMJ <chris666.10@hotmail.com>
2024-03-20 23:33:12 -0500

    DTO Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from '@nestjs/config'; 
+import { UsersModule } from './users/users.module';
 import { MongooseModule }  from '@nestjs/mongoose'; 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { MongooseModule }  from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.urimongo),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
