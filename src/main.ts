75a41b3d9adf5092ff269e6fe1ac62bfcd94d0c5
chrisMJ <chris666.10@hotmail.com>
2024-03-21 14:49:38 -0500

    correcion final main

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filters';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter());
-  app.useGlobalInterceptors (new TimeOutInterceptor());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
+
+  const opciones = new DocumentBuilder()
+    .setTitle('Api-Tarea')
+    .setDescription('Documentacion de Api-Tarea')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, opciones);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
