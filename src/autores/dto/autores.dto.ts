98723d0bd8ea3f5bdbb92d31ac32fdd5687cefbd
chrisMJ <chris666.10@hotmail.com>
2024-03-21 00:11:52 -0500

    DTO Autores

--- src/autores/dto/autores.dto.ts
+++ src/autores/dto/autores.dto.ts
@@ -2,22 +2,22 @@ import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class AutoresDTO {
+export class LibrosDTO {
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
-  readonly nombre: string;
-  @ApiProperty()
-  @IsNotEmpty()
-  @IsString()
-  readonly nacionalidad: string;
-  @ApiProperty()
-  @IsNotEmpty()
-  @Type(() => Date)
-  @IsDate()
-  readonly anio_nacimiento;
+  readonly titulo: string;
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly genero: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  @Type(() => Date)
+  @IsDate()
+  readonly anio_publicacion;
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  readonly editorial: string;
 }
